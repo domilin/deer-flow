f487bb16182afcbe48ba98c4c2caed2415ba2007
ChrisChou <124622297@qq.com>
2025-05-23 15:31:54 +0800

    update on 2025.5.23

--- web/src/lib/utils.ts
+++ web/src/lib/utils.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2025 Bytedance Ltd. and/or its affiliates
 // SPDX-License-Identifier: MIT
 
-import { type ClassValue, clsx } from "clsx"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
